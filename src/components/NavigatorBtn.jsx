ea3c44c3854a8f7bc8bf80048601df409be2f79e
Alexander Klein <agklein04@outlook.com>
2024-10-26 11:40:21 -0400

    Added API and Navigation in Menu

--- src/components/NavigatorBtn.jsx
+++ src/components/NavigatorBtn.jsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 
-export default function NavigatorBtn({ text }) {
+export default function NavigatorBtn({ text, onClick }) {
   return (
     <>
-      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+      <button
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        onClick={onClick} // Attach the onClick event
+      >
         {text}
       </button>
     </>
