0583c91257e31ff40581349b4d10f265b8237b67
Alexander Klein <agklein04@outlook.com>
2024-10-25 23:12:31 -0400

    Added navigator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // App.js
 import React from "react";
 import Map from "./components/Map.jsx";
+import Navigator from "./components/Navigator.jsx";
 
 export default function App() {
   return (
     <div>
+      <Navigator />
       <Map />
     </div>
   );
---
+++ src/components/Navigator.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export default function Navigator() {
+  return (
+    <>
+        <div className="Nav align-center">
+            <div className="text-3xl font-bold mb-2">
+                NaviGator
+            </div>
+            <div className="">
+                I am the navigation panel!
+            </div>
+        </div>
+    </>
+  )
+}
